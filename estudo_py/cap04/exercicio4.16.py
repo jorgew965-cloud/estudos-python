37f549bf9c1db6165b22bf4ac39c2d3fc8ec7072
Jorge <jorgew965@gmail.com>
2026-02-22 01:37:07 -0300

    exercicio4.16 feito
    completo no dia 22/02/2026

---
+++ estudo_py/cap04/exercicio4.16.py
@@ -0,0 +1,15 @@
+# corriga o programa a seguir
+# media = input('digite sua media')
+# if media < 4:
+#   print('Infelizmente vc foi reprovado')
+# if media < 7:
+#   print('vc ficou de recuperação')
+# if media > 7:
+#   print(' vc passou de ano direto')
+media = float(input('Qual a sua media?'))
+if media < 4:
+    print('Infelizmente, vc foi reprovado')
+elif media < 7:
+    print('vc ficou de recuperação')
+else:
+    print('vc passou de ano direto')

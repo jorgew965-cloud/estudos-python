8e8d45ed2151d0ef65ec611509c66a28c2374c02
Jorge <jorgew965@gmail.com>
2026-02-21 22:59:42 -0300

    exercicio 4.14 feito
    completo no dia 21/02/2026

---
+++ estudo_py/cap04/exercicio4.14.py
@@ -0,0 +1,14 @@
+#reescreva o programa o programa a seguir com if-elis-else adicione as linhas nessecarias para ele funcionar
+#if a <10
+#   print(' A e menor q 10')
+#if a >= and a <20
+#   print(' a me maior q 10 e menos q 20')
+# if a >= 20:
+#   print(' A e maior q 20)
+a = int(input(' Digite a'))
+if a >= 20:
+    print('A e maior 20')
+elif  a >= 10 and a <20:
+    print('A e maior q 10 e menos que 20')
+else:
+    print('A e menor que 10')

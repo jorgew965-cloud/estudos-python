91c8aea7389e3550e5a071e0401660a4a8e247fe
Jorge <jorgew965@gmail.com>
2026-02-21 22:07:13 -0300

    exercicio4.13 resolvido
    completo no dia 21/02/2026

---
+++ estudo_py/cap04/exercicio4.13.py
@@ -0,0 +1,11 @@
+# no programa a seguir, inverta as linhas if e else negando a condiçção. adicione linhas necessarias para faze-lo funcionar em python
+# if a > b:
+#   print('A e maior que b')
+#else:
+#   print(' b e maior que a')
+a = int(input('Digite A'))
+b = int(input('Digite B'))
+if a <= b:
+    print('B e maior que a')
+else:
+    print('A e maior que B')
